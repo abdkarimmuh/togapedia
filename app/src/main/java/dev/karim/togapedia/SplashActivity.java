2fc820a1354333c5e12128ebd7c203fe6f90ad99
setakarim <alkariim.tridek@gmail.com>
2018-01-02 17:26:00 +0700

    first commit

---
+++ app/src/main/java/dev/karim/togapedia/SplashActivity.java
@@ -0,0 +1,36 @@
+package dev.karim.togapedia;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static boolean splashLoaded = false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!splashLoaded) {
+            setContentView(R.layout.activity_splash);
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 3000);
+
+            splashLoaded = true;
+        }
+        else {
+            Intent goToMainActivity = new Intent(SplashActivity.this, MainActivity.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
+    }
+}
